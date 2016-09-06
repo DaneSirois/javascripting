c7f92e2f1159f68f155ecc79daf4ffbb5490d837
Dane Sirois <dane_sirois@live.com>
2016-09-06 03:16:17 +0000

    Initial commit with all my work so far

---
+++ javascripting/function.arguments.js
@@ -0,0 +1,4 @@
+var math = function(hi, hello, bye) {
+	return hello * bye + hi
+}
+console.log(math(53, 61, 67));
---
+++ javascripting/revising-strings.js
@@ -0,0 +1,4 @@
+var pizza = 'pizza is alright'
+var replace = pizza.replace("alright", "wonderful")
+console.log(replace);
+
